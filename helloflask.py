dd40418003bed1d3577d333052dac320d3151b4b
Agostinho OURIQUE-CRUZ <aourique-cruz@amadeus.com>
2017-07-08 01:11:54 +0200

    3rd Test

--- helloflask.py
+++ helloflask.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "Hello World! Agostinho Ourique Cruz"
+   return "Hello World! This Sucks!!!"
 
 if __name__ == "__main__":
    app.run()
